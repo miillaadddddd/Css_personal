2712c7593c7ca6bfc993aed5d6b786a2a175df1a
miillaadddddd <milad7212@gmail.com>
2021-09-05 17:41:50 +0430

    forhome

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// ------------------about section tabs------------------
+
+(() => {
+    const aboutSection = document.querySelector(".about-section");
+    tabsContainer = document.querySelector(".about-tabs");
+    console.log(`aboutSection`, aboutSection);
+    console.log(`tabsContainer`, tabsContainer);
+    tabsContainer.addEventListener("click")
+})();
